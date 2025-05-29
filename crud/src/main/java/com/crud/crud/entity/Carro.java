7269bae76d0454e6d5d592c4a2a1aa73d8ee838f
felip-SN <felipenascimentooudri@gmail.com>
2025-05-28 22:37:40 -0300

    Criando tabelas e suas relações para estudar

--- crud/src/main/java/com/crud/crud/entity/Carro.java
+++ crud/src/main/java/com/crud/crud/entity/Carro.java
@@ -1,10 +1,26 @@
 package com.crud.crud.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Carro {
     @Id
@@ -13,22 +29,11 @@ public class Carro {
     private String name;
     private int ano;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAno() {
-        return ano;
-    }
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("carros")
+    private Marca marca;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "carro_proprietario")
+    private List<Proprietario> proprietarios;
 }
